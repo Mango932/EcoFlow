353817da12a1b8a2f46217fc1212cb1c2967e387
Patrick Bonini <patrick.bonini13@gmail.com>
2024-03-02 11:02:54 -0500

    add explore_data.py

---
+++ model/explore_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class CropDataAnalysis:
+    def __init__(self, file_path):
+        self.dataset = pd.read_csv(file_path)
+    
+    def calculate_correlation(self):
+        return self.dataset.drop(['label'], axis=1).corr()
+    
+    def plot_heatmap(self, data):
+        heatmap = sns.heatmap(data, annot=True, cbar=True, cmap='coolwarm')
+        plt.show()
+    
+    def crop_values_count(self):
+        return self.dataset['label'].value_counts()
+    
+    def plot_nitrogen_distribution(self):
+        displot = sns.displot(self.dataset['N'])
+        plt.show()
+
+file_path = "dataset\Crop_recommendation.csv"
+crop_analysis = CropDataAnalysis(file_path)
+
+correlation_data = crop_analysis.calculate_correlation()
+print("Correlation:\n", correlation_data, "\n")
+
+crop_analysis.plot_heatmap(correlation_data)
+
+crop_values = crop_analysis.crop_values_count()
+print("Crop values count:\n", crop_values, "\n")
+
+crop_analysis.plot_nitrogen_distribution()
---
+++ model/load_and_display_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+class CropDataset:
+    def __init__(self, file_path):
+        self.dataset = pd.read_csv(file_path)
+    
+    def get_num_rows(self):
+        return self.dataset.shape[0]
+    
+    def get_info(self):
+        return self.dataset.info()
+    
+    def check_null_values(self):
+        return self.dataset.isnull().sum()
+    
+    def check_duplicates(self):
+        return self.dataset.duplicated().sum()
+    
+    def get_statistics(self):
+        return self.dataset.describe()
+
+file_path = "dataset\Crop_recommendation.csv"
+crop_dataset = CropDataset(file_path)
+
+print("Num of rows:", crop_dataset.get_num_rows(), "\n")
+print("Dataset info:\n", crop_dataset.get_info(), "\n")
+print("Null values:\n", crop_dataset.check_null_values(), "\n")
+print("Duplicates:", crop_dataset.check_duplicates(), "\n")
+print("Statistics:\n", crop_dataset.get_statistics(), "\n")
