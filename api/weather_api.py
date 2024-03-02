a7abd514370d1ebd85c8bf6f17f5ddde0cfc5e69
MartinPatr <martinpatrouchev@gmail.com>
2024-03-02 14:38:29 -0500

    add python file to get average weather statistics

---
+++ api/weather_api.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+# Get the weather data for a given latitude and longitude
+def get_weather_data(lat, lon):
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{lat},{lon}/lastyear?unitGroup=metric&include=days&key={get_APIKEY()}&contentType=json"
+
+    print(url)
+
+    response = requests.get(url)
+    data = response.json()
+
+    return get_averages(data)
+
+# Get the average weather data from the weather data
+def get_averages(data):
+    days = data['days']
+
+    averages ={
+        'temperature': 0,
+        'rainfall': 0,
+        'humidity': 0,
+    }
+
+    for day in days:
+        averages['temperature'] += day['temp']
+        averages['rainfall'] += day['precip']
+        averages['humidity'] += day['humidity']
+
+    averages['temperature'] /= len(days)
+    averages['rainfall'] /= 10
+    averages['humidity'] /= len(days)
+
+
+    return averages
+
+
+# Get the API key from the apikey.json file
+def get_APIKEY():
+    with open('model/apikey.json') as f:
+        data = json.load(f)
+    api_key = data['APIKEY']
+    return api_key
